ca9e3ece53148f1f6ad12342c3ef5829563ba5cb
Abed Ayoubi <Mohammed.Abed@xmpie.com>
2022-05-22 13:19:00 +0300

    fix(jquery) move configuration into common file for jquery examples

---
+++ Sample sites/jquery/xmpcfg.js
@@ -0,0 +1,8 @@
+var xmpcfg = {
+    access: { 
+      accessToken: '8ae058d5-31f0-4e12-8a47-9f82fc6df6ef_2d9015c5-f545-4955-9194-8a9253f2bea7_f4a37cbbdc5f482e80a11810de87be2d', 
+      url: 'http://52.213.0.23/XMPieXMPL_REST_API', 
+      circleProjectID:'2d9015c5-f545-4955-9194-8a9253f2bea7', 
+      circleProjectName: 'XMPL SDK Example - New2022 - Login' 
+    }
+  }
