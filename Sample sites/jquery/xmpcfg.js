50428eaa4e9422fd0eda1517897694e3c4bd5b82
Anastasiia Tsvetkova <Anastasiia.Tsvetkova@xmpie.com>
2022-06-03 12:53:35 +0300

    Fix cdn url in examples

--- Sample sites/jquery/xmpcfg.js
+++ Sample sites/jquery/xmpcfg.js
@@ -1,8 +1,1 @@
-var xmpcfg = {
-    access: { 
-      accessToken: '8ae058d5-31f0-4e12-8a47-9f82fc6df6ef_2d9015c5-f545-4955-9194-8a9253f2bea7_f4a37cbbdc5f482e80a11810de87be2d', 
-      url: 'http://52.213.0.23/XMPieXMPL_REST_API', 
-      circleProjectID:'2d9015c5-f545-4955-9194-8a9253f2bea7', 
-      circleProjectName: 'XMPL SDK Example - New2022 - Login' 
-    }
-  }
+var xmpcfg = {access: { accessToken: '96746f5e-af1b-465b-b5ca-abe05cbc247b_eab517f1-ecea-4926-a97a-1d862ba808f3_8f5265ca6ad84b1492e18f2547a4a7ee', url: 'http://xmpl02.xmpie.net/XMPieXMPL_REST_API', circleProjectID:'eab517f1-ecea-4926-a97a-1d862ba808f3', circleProjectName: 'XMPL_examples' }};
