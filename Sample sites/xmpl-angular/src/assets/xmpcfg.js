fc8106bedf8ff7104bdd6b24721791ee5a1fab44
Denys Sakharov <Denys.Sakharov@xmpie.com>
2023-07-24 12:17:24 +0300

    added sample config files

---
+++ Sample sites/xmpl-angular/src/assets/xmpcfg.js
@@ -0,0 +1,8 @@
+var xmpcfg = {
+    access: {
+        accessToken: '8ae058d5-31f0-4e12-8a47-9f82fc6df6ef_1fdb5415-794f-4e1a-9912-093fc810bc64_e299a53d478a4276a769a8d815db4cc4',
+        url: 'http://176.34.252.52/XMPieXMPL_REST_API',
+        circleProjectID: '1fdb5415-794f-4e1a-9912-093fc810bc64',
+        circleProjectName: 'Sample'
+    }
+};
